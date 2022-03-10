cf6c63681da799e2fb2ff96f792b51d6f99163dd
Renato Teixeira <rena2damas@gmail.com>
2022-03-10 19:43:00 +0000

    Add app runner

---
+++ src/app.py
@@ -0,0 +1,86 @@
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_plugins.webframeworks.flask import FlaskPlugin
+from flasgger import apispec_to_template, Swagger
+from flask import Blueprint, Flask, redirect, url_for
+
+from filexplorer import __meta__, __version__
+from src.resources.health_checks import blueprint as health_checks
+from src.settings import oas
+from src.settings.env import config_class, load_dotenv
+
+
+def create_app(config_name="development", dotenv=True, configs=None):
+    """Create a new app."""
+
+    # define the WSGI application object
+    app = Flask(__name__, static_folder=None)
+
+    # load object-based default configuration
+    load_dotenv(dotenv)
+    app.config.from_object(config_class(config_name))
+    app.config.update(configs or {})
+
+    setup_app(app)
+
+    return app
+
+
+def setup_app(app):
+    """Initial setups."""
+    url_prefix = app.config["APPLICATION_ROOT"]
+    openapi_version = app.config["OPENAPI"]
+
+    # initial blueprint wiring
+    index = Blueprint("index", __name__)
+    index.register_blueprint(health_checks)
+    app.register_blueprint(index, url_prefix=url_prefix)
+
+    # base template for OpenAPI specs
+    oas.converter = oas.create_spec_converter(openapi_version)
+    spec_template = oas.base_template(
+        openapi_version=openapi_version,
+        info={
+            "title": __meta__["name"],
+            "version": __version__,
+            "description": __meta__["summary"],
+        },
+        servers=[oas.Server(url=url_prefix, description=app.config["ENV"])],
+        tags=[
+            oas.Tag(
+                name="filesystem",
+                description="CRUD operations over files in current filesystem",
+            )
+        ],
+        responses=[
+            oas.HttpResponse(code=404, reason="NotFound", description="Not Found")
+        ],
+    )
+
+    spec = APISpec(
+        title=__meta__["name"],
+        version=__version__,
+        openapi_version=openapi_version,
+        plugins=(FlaskPlugin(), MarshmallowPlugin()),
+        basePath=url_prefix,
+        **spec_template
+    )
+
+    # create paths from app views
+    for view in app.view_functions.values():
+        spec.path(view=view, app=app, base_path=url_prefix)
+
+    # generate swagger from spec
+    Swagger(
+        app=app,
+        config=oas.swagger_configs(
+            openapi_version=openapi_version, app_root=url_prefix
+        ),
+        template=apispec_to_template(app=app, spec=spec),
+        merge=True,
+    )
+
+    # redirect root path to context root
+    app.add_url_rule(
+        "/", "index", view_func=lambda: redirect(url_for("flasgger.apidocs"))
+    )
--- src/filexplorer/__init__.py
+++ src/filexplorer/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata  # python<=3.7
+
+__meta__ = metadata.metadata("filexplorer")
+__version__ = __meta__["version"]
