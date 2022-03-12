78b856ca044f84ed649af0c16e934290870ff8b1
Renato Teixeira <rena2damas@gmail.com>
2022-03-12 16:17:13 +0000

    Use /tmp as a test directory

--- tests/functional/test_filesystem.py
+++ tests/functional/test_filesystem.py
@@ -5,13 +5,13 @@ from src.api.auth import AuthAPI
 
 class TestFilesystem:
     def test_unauthorized_request_throws_401(self, client):
-        response = client.get("/filesystem/", headers={})
+        response = client.get("/filesystem/tmp", headers={})
         assert response.status_code == 401
 
     def test_authorized_request_returns_200(self, client, mocker):
         mocker.patch.object(AuthAPI, "authenticate", return_value=True)
         headers = {"Authorization": f"Basic {base64.b64encode(b'user:pass').decode()}"}
-        response = client.get("/filesystem/", headers=headers)
+        response = client.get("/filesystem/tmp", headers=headers)
         assert response.status_code == 200
 
     def test_supported_paths(self, client):
