db11c63176bd6bcde67f74bf6d9c57e27ba73ece
Renato Teixeira <rena2damas@gmail.com>
2022-03-12 09:41:07 +0000

    Test authorization

--- tests/functional/test_filesystem.py
+++ tests/functional/test_filesystem.py
@@ -1,5 +1,19 @@
-from src import __version__
+import base64
+
+from src.api.auth import AuthAPI
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+class TestFilesystem:
+    def test_unauthorized_request_throws_401(self, client):
+        response = client.get("/filesystem/", headers={})
+        assert response.status_code == 401
+
+    def test_authorized_request_returns_200(self, client, mocker):
+        mocker.patch.object(AuthAPI, "authenticate", return_value=True)
+        headers = {"Authorization": f"Basic {base64.b64encode(b'user:pass').decode()}"}
+        response = client.get("/filesystem/", headers=headers)
+        assert response.status_code == 200
+
+    def test_supported_paths(self, client):
+        response = client.get("/filesystem/supported-paths")
+        assert response.status_code == 200
