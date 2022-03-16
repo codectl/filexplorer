c2c2272ccd3dd8f944f804a5b9673e610716a15b
Renato Teixeira <rena2damas@gmail.com>
2022-03-16 16:28:05 +0000

    Validate pam success

--- src/api/auth.py
+++ src/api/auth.py
@@ -6,4 +6,4 @@ __all__ = ("AuthAPI",)
 class AuthAPI:
     @staticmethod
     def authenticate(username, password):
-        return pam.authenticate(username, password)
+        return pam.authenticate(username, password) == pam.PAM_SUCCESS
