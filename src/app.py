ebfb81d3e7a12b85b8b2e980d69744f9ff419a52
Renato Teixeira <rena2damas@gmail.com>
2022-03-16 18:18:57 +0000

    Ensure app errors are json type

--- src/app.py
+++ src/app.py
@@ -2,8 +2,9 @@ from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_plugins.webframeworks.flask import FlaskPlugin
 from apispec_ui.flask import Swagger
-
-from flask import Blueprint, Flask, redirect, url_for
+from flask import Blueprint, Flask, jsonify, redirect, url_for
+from werkzeug.exceptions import HTTPException
+from werkzeug.http import HTTP_STATUS_CODES
 
 from src import __meta__, __version__
 from src.resources.filesystem import blueprint as filesystem
@@ -39,6 +40,7 @@ def setup_app(app):
 
     # base template for OpenAPI specs
     oas.converter = oas.create_spec_converter(openapi_version)
+
     spec_template = oas.base_template(
         openapi_version=openapi_version,
         info={
@@ -55,11 +57,9 @@ def setup_app(app):
             )
         ],
         responses=[
-            oas.HttpResponse(
-                code=401, reason="Unauthorized", description="Unauthorized"
-            ),
-            oas.HttpResponse(code=404, reason="NotFound", description="Not Found"),
-        ],
+            oas.HttpResponse(code=401, reason=HTTP_STATUS_CODES[401]),
+            oas.HttpResponse(code=404, reason=HTTP_STATUS_CODES[404]),
+        ]
     )
 
     spec = APISpec(
@@ -79,3 +79,9 @@ def setup_app(app):
 
     # redirect root path to context root
     app.add_url_rule("/", "index", view_func=lambda: redirect(url_for("swagger.ui")))
+
+    # jsonify http errors
+    app.register_error_handler(HTTPException, lambda ex: jsonify(oas.HttpResponse(
+        code=ex.code,
+        reason=HTTP_STATUS_CODES[ex.code]
+    )))
