10200435cc1d3056d013681889ca4d07dabd5c49
Renato Teixeira <rena2damas@gmail.com>
2022-03-18 11:25:22 +0000

    Use url_prefix

--- src/settings/oas.py
+++ src/settings/oas.py
@@ -74,8 +74,9 @@ def base_template(
 def swagger_configs(app_root="/"):
     prefix = "" if app_root == "/" else app_root
     return {
-        "swagger_route": prefix + "/",
-        "swagger_static": "/static/",
+        "url_prefix": prefix,
+        "swagger_route": "/",
+        "swagger_static": "/static",
         "swagger_favicon": "favicon.ico",
         "swagger_hide_bar": True,
     }
