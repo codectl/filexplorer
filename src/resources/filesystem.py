d66e9e490b0dc2d5ce72163a09ead5514c935ecc
Renato Teixeira <rena2damas@gmail.com>
2022-03-19 10:10:18 +0000

    Respond with proper http code based on exception

--- src/resources/filesystem.py
+++ src/resources/filesystem.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, jsonify
 from flask_restful import abort, Api, Resource
-from shell import CommandError
 from werkzeug.http import HTTP_STATUS_CODES
 
-from src.api.filesystem import FilesystemAPI
+from src.api.filesystem import (
+    DefaultException,
+    FileNotFoundException,
+    PermissionDeniedException,
+    FilesystemAPI
+)
 from src.resources.auth import current_username, requires_auth
 
 blueprint = Blueprint("filesystem", __name__, url_prefix="/filesystem")
@@ -40,6 +44,8 @@ class Filesystem(Resource):
                 $ref: "#/components/responses/BadRequest"
             401:
                 $ref: "#/components/responses/Unauthorized"
+            403:
+                $ref: "#/components/responses/Forbidden"
             404:
                 $ref: "#/components/responses/NotFound"
         """
@@ -48,11 +54,16 @@ class Filesystem(Resource):
         fs_api = FilesystemAPI(username=username)
         if not any(path.startswith(p) for p in fs_api.supported_paths()):
             abort(
-                400, code=400, reason=HTTP_STATUS_CODES[400], message="Unsupported path"
+                400, code=400, reason=HTTP_STATUS_CODES[400], message="unsupported path"
             )
+        result = []
         try:
             result = fs_api.ls(path=path)
-        except CommandError as ex:
+        except PermissionDeniedException:
+            abort(403, code=403, reason=HTTP_STATUS_CODES[403], message="")
+        except FileNotFoundException:
+            abort(404, code=404, reason=HTTP_STATUS_CODES[404], message="")
+        except DefaultException as ex:
             abort(400, code=400, reason=HTTP_STATUS_CODES[400], message=str(ex))
         return jsonify(result)
 
