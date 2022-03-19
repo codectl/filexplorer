8677c0a3033f80a2b839cf3a55af3cdb98bec5e7
Renato Teixeira <rena2damas@gmail.com>
2022-03-19 10:34:10 +0000

    Helper to create oas serializable responses

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,16 @@
+from werkzeug.http import HTTP_STATUS_CODES
+
+from src.schemas.serlializers.http import HttpResponseSchema
+from src.settings import oas
+
+
 def clean_sh_error(error):
     """Get a clean error returned by a shell command."""
     return error.split(":")[-1].strip()
+
+
+def http_response(code: int, serialize=True, **kwargs):
+    response = oas.HttpResponse(code=code, reason=HTTP_STATUS_CODES[code])
+    if serialize:
+        return HttpResponseSchema(**kwargs).dump(response)
+    return response
