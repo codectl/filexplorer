7e43ad2080f7845d7d71c27089a9fa7cf594a49a
Renato Teixeira <rena2damas@gmail.com>
2022-03-20 13:23:21 +0000

    Fix & extend unit tests

--- tests/unit/test_filesystem.py
+++ tests/unit/test_filesystem.py
@@ -1,9 +1,12 @@
 import pytest
 from flask import Flask
-from shell import CommandError, Shell
 
-from src.api.filesystem import FilesystemAPI
-from tests.utils import MockShell
+from src.api.filesystem import (
+    DefaultException,
+    FileNotFoundException,
+    PermissionDeniedException,
+    FilesystemAPI,
+)
 
 
 @pytest.fixture(scope="class")
@@ -12,21 +15,43 @@ def api():
 
 
 class TestFilesystemAPI:
-    def test_supported_paths(self, api):
+    def test_supported_paths_returns_values(self, api):
         app = Flask(__name__)
         app.config["SUPPORTED_PATHS"] = ["/test"]
         with app.test_request_context():
             assert api.supported_paths() == ["/test"]
 
-    def test_ls(self, api, mocker):
-        mock_shell = MockShell(code=0)
-        mock_shell.output = lambda: "valid"
-        mocker.patch.object(Shell, "run", return_value=mock_shell)
-        assert api.ls(path="/valid-path") == "valid"
+    @pytest.mark.parametrize(
+        "mocker_shell",
+        [(0, ["file"], "")],
+        indirect=True,
+    )
+    def test_valid_ls_returns_valid_data(self, api, mocker_shell):
+        assert api.ls(path="/valid-path") == ["file"]
 
-        mock_shell = MockShell(code=1)
-        mock_shell.errors = lambda **_: "error"
-        mocker.patch.object(Shell, "run", return_value=mock_shell)
-        with pytest.raises(CommandError) as ex:
-            assert api.ls(path="/invalid-path")
-            assert str(ex) == "error"
+    @pytest.mark.parametrize(
+        "mocker_shell",
+        [(1, [], "ls: root: Permission denied")],
+        indirect=True,
+    )
+    def test_ls_on_restricted_file_raises_exception(self, api, mocker_shell):
+        with pytest.raises(PermissionDeniedException):
+            assert api.ls(path="/restricted-path")
+
+    @pytest.mark.parametrize(
+        "mocker_shell",
+        [(1, [], "ls: /tmp/missing-file: No such file or directory")],
+        indirect=True,
+    )
+    def test_ls_on_missing_file_raises_exception(self, api, mocker_shell):
+        with pytest.raises(FileNotFoundException):
+            assert api.ls(path="/missing-file")
+
+    @pytest.mark.parametrize(
+        "mocker_shell",
+        [(1, [], "ls: ???")],
+        indirect=True,
+    )
+    def test_ls_generic_error_raises_exception(self, api, mocker_shell):
+        with pytest.raises(DefaultException):
+            assert api.ls(path="/???")
