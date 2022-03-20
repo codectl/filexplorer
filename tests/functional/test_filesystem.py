b0422def53a491d31cbda3244236019bd32747d1
Renato Teixeira <rena2damas@gmail.com>
2022-03-20 13:22:59 +0000

    Rename mocker shell

--- tests/functional/test_filesystem.py
+++ tests/functional/test_filesystem.py
@@ -25,31 +25,31 @@ class TestFilesystem:
         assert response.status_code == 400
 
     @pytest.mark.parametrize(
-        "mock_shell",
+        "mocker_shell",
         [(0, ["file"], "")],
         indirect=True,
     )
-    def test_valid_path_returns_200(self, client, auth, mock_shell):
+    def test_valid_path_returns_200(self, client, auth, mocker_shell):
         response = client.get("/filesystem/tmp/", headers=auth)
         assert response.status_code == 200
         assert response.json == ["file"]
 
     @pytest.mark.parametrize(
-        "mock_shell",
+        "mocker_shell",
         [(1, [], "ls: root: Permission denied")],
         indirect=True,
     )
-    def test_permission_denied_returns_403(self, client, auth, mock_shell):
+    def test_permission_denied_returns_403(self, client, auth, mocker_shell):
         response = client.get("/filesystem/tmp/root/", headers=auth)
         assert response.status_code == 403
         assert response.json == {"code": 403, "message": "", "reason": "Forbidden"}
 
     @pytest.mark.parametrize(
-        "mock_shell",
+        "mocker_shell",
         [(1, [], "ls: /tmp/invalid/: No such file or directory")],
         indirect=True,
     )
-    def test_missing_resource_returns_404(self, client, auth, mock_shell):
+    def test_missing_resource_returns_404(self, client, auth, mocker_shell):
         response = client.get("/filesystem/tmp/invalid/", headers=auth)
         assert response.status_code == 404
         assert response.json == {"code": 404, "message": "", "reason": "Not Found"}
