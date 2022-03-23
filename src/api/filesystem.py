c3fe40200d4c60856b1ae34681cfe181d8017efd
Renato Teixeira <rena2damas@gmail.com>
2022-03-23 19:36:38 +0000

    Drop shell utility & use tee for file creation

--- src/api/filesystem.py
+++ src/api/filesystem.py
@@ -1,5 +1,7 @@
+import subprocess
+
 from flask import current_app
-from shell import CommandError, shell
+from werkzeug.utils import secure_filename
 
 from src import utils
 
@@ -8,21 +10,43 @@ __all__ = ("FilesystemAPI",)
 
 class FilesystemAPI:
     def __init__(self, username=None):
-        self.username = username
+        self.username = str(username) if username else None
 
     def ls(self, path):
-        path = utils.validate_path(path, mode="r")
-        command = f"ls {path}"
-        return self._run(command)
+        return self._run(cmd=f"ls {path}", user=self.username)
 
-    def sudo(self, command):
-        return f"sudo -u {self.username} {command}"
+    def create(self, path, files=()):
+        path_files = self.ls(path)
+        if any(secure_filename(file.filename) in path_files for file in files):
+            raise FileExistsError(f"file already exists in given path")
+        for file in files:
+            filename = secure_filename(file.filename)
+            dst = f"{path}/{filename}"
+            self._run(
+                cmd=f"tee {dst}",
+                stdin=file,
+                stdout=subprocess.DEVNULL,
+                user=self.username
+            )
 
-    def _run(self, command):
-        process = shell(self.sudo(command))
-        if process.code > 0:
-            raise CommandError(process.errors(raw=True))
-        return process.output()
+    @classmethod
+    def _run(cls, cmd, **kwargs):
+        try:
+            stdout = utils.shell(cmd, **kwargs)
+        except subprocess.CalledProcessError as ex:
+            cls.raise_error(ex.stderr)
+        else:
+            return stdout.splitlines() if stdout else []
+
+    @staticmethod
+    def raise_error(stderr):
+        err = stderr.split(":")[-1].strip().lower()
+        if err == "no such file or directory":
+            raise FileNotFoundError(err)
+        elif err == "permission denied":
+            raise PermissionError(err)
+        else:
+            raise Exception(err)
 
     @staticmethod
     def supported_paths():
