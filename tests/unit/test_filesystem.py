878493d1a7e9cba3355832f5d46e1ac2bc45600e
Renato Teixeira <rena2damas@gmail.com>
2022-03-25 20:59:40 +0000

    Test error cases for attachment method

--- tests/unit/test_filesystem.py
+++ tests/unit/test_filesystem.py
@@ -22,22 +22,22 @@ class TestFilesystemAPI:
 
     def test_valid_ls_returns_valid_data(self, api, mocker):
         mocker.patch("src.utils.shell", return_value="file.txt")
-        assert api.ls(path="/valid-path") == ["file.txt"]
+        assert api.ls(path="/tmp/") == ["file.txt"]
 
     def test_ls_on_restricted_path_raises_exception(self, api, mocker):
         stderr = "ls: /tmp/root/: Permission denied"
         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
         mocker.patch("src.utils.shell", side_effect=err)
         with pytest.raises(PermissionError) as ex:
-            assert api.ls(path="/restricted-path")
+            assert api.ls(path="/tmp/root/")
         assert str(ex.value) == "permission denied"
 
     def test_ls_on_missing_file_raises_exception(self, api, mocker):
-        stderr = "ls: /tmp/missing/: No such file or directory"
+        stderr = "ls: /tmp/file.txt: No such file or directory"
         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
         mocker.patch("src.utils.shell", side_effect=err)
         with pytest.raises(FileNotFoundError) as ex:
-            assert api.ls(path="/missing")
+            assert api.ls(path="/tmp/file.txt")
         assert str(ex.value) == "no such file or directory"
 
     def test_ls_on_error_raises_exception(self, api, mocker):
@@ -45,17 +45,62 @@ class TestFilesystemAPI:
         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
         mocker.patch("src.utils.shell", side_effect=err)
         with pytest.raises(Exception) as ex:
-            assert api.ls(path="/???")
+            assert api.ls(path="/tmp/error")
         assert str(ex.value) == stderr
 
     def test_valid_file_attachment(self, api, mocker):
         mocker.patch("src.utils.shell", return_value=b"content")
-        name, content = api.attachment(path="/file.txt", mode=stat.S_IFREG)
+        name, content = api.attachment(path="/tmp/file.txt", mode=stat.S_IFREG)
         assert name == "file.txt"
         assert content.read() == io.BytesIO(b"content").read()
 
     def test_valid_directory_attachment(self, api, mocker):
         mocker.patch("src.utils.shell", return_value=b"content")
-        name, content = api.attachment(path="/dir/", mode=stat.S_IFDIR)
+        name, content = api.attachment(path="/tmp/dir/", mode=stat.S_IFDIR)
         assert name == "dir.tar.gz"
         assert content.read() == io.BytesIO(b"content").read()
+
+    def test_restricted_file_attachment_raises_exceptions(self, api, mocker):
+        stderr = "tar: Couldn't list extended attributes: Permission denied"
+        err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
+        mocker.patch("src.utils.shell", side_effect=err)
+        with pytest.raises(PermissionError) as ex:
+            assert api.attachment(path="/tmp/file.txt", mode=stat.S_IFREG)
+        assert str(ex.value) == "permission denied"
+
+    def test_restricted_directory_attachment_raises_exceptions(self, api, mocker):
+        stderr = "tar: Couldn't list extended attributes: Permission denied"
+        err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
+        mocker.patch("src.utils.shell", side_effect=err)
+        with pytest.raises(PermissionError) as ex:
+            assert api.attachment(path="/tmp/dir/", mode=stat.S_IFDIR)
+        assert str(ex.value) == "permission denied"
+
+    def test_invalid_mode_attachment_raises_exceptions(self, api, mocker):
+        with pytest.raises(ValueError) as ex:
+            assert api.attachment(path="", mode=stat.S_IFCHR)
+        assert str(ex.value) == "unsupported file mode"
+
+    def test_missing_file_attachment_raises_exceptions(self, api, mocker):
+        stderr = "tar: /tmp/file.txt: Cannot stat: No such file or directory"
+        err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
+        mocker.patch("src.utils.shell", side_effect=err)
+        with pytest.raises(FileNotFoundError) as ex:
+            assert api.ls(path="/tmp/file.txt")
+        assert str(ex.value) == "no such file or directory"
+
+    def test_missing_directory_attachment_raises_exceptions(self, api, mocker):
+        stderr = "tar: /tmp/dir/: Cannot stat: No such file or directory"
+        err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
+        mocker.patch("src.utils.shell", side_effect=err)
+        with pytest.raises(FileNotFoundError) as ex:
+            assert api.ls(path="/tmp/dir/")
+        assert str(ex.value) == "no such file or directory"
+
+    def test_error_attachment_raises_exceptions(self, api, mocker):
+        stderr = "tar: some error occurred"
+        err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
+        mocker.patch("src.utils.shell", side_effect=err)
+        with pytest.raises(Exception) as ex:
+            assert api.ls(path="/tmp/dir/")
+        assert str(ex.value) == "some error occurred"
